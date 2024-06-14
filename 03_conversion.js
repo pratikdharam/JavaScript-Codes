4596588205b61c5626c1a7af9d48e92af67b5412
Pratik <134490147+pratikdp03@users.noreply.github.com>
2024-06-14 23:16:55 +0530

    Create 03_conversion.js

---
+++ 03_conversion.js
@@ -0,0 +1,28 @@
+// let convert into number first
+
+let firstName = "Pratik12"
+
+let nameInNumber = Number(firstName)
+
+console.log(nameInNumber);
+console.log(typeof nameInNumber);
+
+//"33"=> 33
+//"pratik33"=> NaN(Not a number)
+//true=1, false=0
+
+//let convert into boolean
+
+let a = "Pratik"
+
+console.log(typeof a);
+
+let b = Boolean(a)
+
+console.log(typeof b);
+console.log(b)
+
+//"Pratik"=> true
+//""=> false
+//1=>true , 0=> false
+
