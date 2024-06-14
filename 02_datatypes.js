b734e2bd71454e2582b49572589e156b97631e89
Pratik <134490147+pratikdp03@users.noreply.github.com>
2024-06-14 20:00:26 +0530

    Create 02_datatypes.js
    learned about datatypes

---
+++ 02_datatypes.js
@@ -0,0 +1,10 @@
+let name ="Pratik"
+let age = 20
+let email;
+let city = null
+
+
+console.log(typeof 22)//number
+console.log(typeof "Pratiiikkkk"); //string
+console.log(typeof undefined) //undefined
+console.log(typeof null); //object
