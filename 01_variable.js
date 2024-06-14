f6497bbe9849068cbaabd482d00c2ee0da978314
Pratik <134490147+pratikdp03@users.noreply.github.com>
2024-06-14 12:36:25 +0530

    Create 01_variable.js
    learned about variables

---
+++ 01_variable.js
@@ -0,0 +1,15 @@
+const Name = "Pratik"
+
+let Address = "Shirdi"
+
+var City = "Rahata"
+
+mobileNo = "9561901412"
+
+let rollNO;
+
+//dont use var because of scope problem in js
+
+console.log(Name);
+
+console.table([Address, City, mobileNo, rollNO]);
