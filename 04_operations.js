ff2ade279fcc3e34a54ce2cd1e8488abbdad9d04
Pratik <134490147+pratikdp03@users.noreply.github.com>
2024-06-15 10:54:12 +0530

    Create 04_operations.js
    learned about operations and prefix and postfix operators also

---
+++ 04_operations.js
@@ -0,0 +1,53 @@
+let a = 3
+
+let neg_a = -a
+
+// console.log(neg_a);
+
+//simple operations
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2/2);
+// console.log(2%3);
+// console.log(2*2);
+// console.log(2**3);
+
+
+
+
+//addtion of string and number
+
+let str1 = "Pratik"
+let str2 = " Dhraram"
+
+let str3 = str1 + str2
+
+// console.log(str3);
+
+// console.log(1+1);
+// console.log("1" + 1); // string + number = string   
+// console.log(1 + "1"); // number + string = string
+// console.log(1 + 1 + "1"); //number + number = number + string = string
+// console.log("1" + 1 + 1); 
+
+
+//prefix and postfix operator
+
+
+
+//prefix------ in this value of variavle that we declare as first in incremented get increments and new varailble got old value
+
+// let score = 4
+
+// let new_score = score++
+
+// console.log("score: " + score + " new_score" + new_score);
+
+// postfix----- in this new varible and old variable two get incremented 
+
+// let score = 4
+
+// let new_score = ++score
+
+// console.log("score: " + score + " new_score" + new_score);
