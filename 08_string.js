9f24830a38dc9a8b686501a0a41e77e56194b193
Pratik <134490147+pratikdp03@users.noreply.github.com>
2024-06-17 16:37:17 +0530

    Create 08_string.js
    learned all about strings

---
+++ 08_string.js
@@ -0,0 +1,30 @@
+let firstName = "Pratik"
+
+let lastName = "Dharam"
+
+// console.log(`Hey my name is ${firstName} and my surnaae is ${lastName} `);
+
+
+let gameName = new String ('Pratik_new_server')
+
+
+// console.log(gameName.toUpperCase());
+
+
+// console.log(gameName.length);
+
+// console.log(gameName.charAt(3));
+
+let name = "     pratik"
+
+// console.log(name);
+// console.log(name.trim());
+
+const url = "github.com/pratik%20dharam/javascript-codes"
+
+console.log(url.replace('%20', '-'));
+
+console.log(url.includes('pratik'));
+console.log(url.includes('ajinkya'));
+
+console.log(gameName.split('_'));
