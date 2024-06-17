dd313b6e27e7558cd15b6a9bec56e3a1caa27ef2
Pratik <134490147+pratikdp03@users.noreply.github.com>
2024-06-17 23:02:27 +0530

    Create 09_numbers_and_math.js
    changed for experiment

---
+++ 09_numbers_and_math.js
@@ -0,0 +1,45 @@
+const score = new Number(400)
+
+// console.log(score);
+
+
+// console.log(score.toString().length);
+
+// console.log(score.toFixed(2));
+
+let newScore = 124.55
+
+// console.log(newScore.toPrecision(4));
+
+let anotherScore = 10000000000
+
+// console.log(anotherScore.toLocaleString('en-IN'));
+
+
+//math code
+
+
+// console.log(Math);
+
+// console.log(Math.round(7.9));
+// console.log(Math.floor(7.9));
+// console.log(Math.ceil(7.1));
+
+// console.log(Math.min(4,5,2,6,7,1));
+// console.log(Math.max(4,5,2,6,7,1));
+
+// console.log(Math.abs(-4));
+
+//random stuff
+//random function generates values between 0 to 1
+
+// console.log(Math.floor(Math.random() * 10) + 1) ;                    // genreating random values from 1 to 10
+
+//we got 1 formula if we wnat min and max values
+
+let min = 10
+let max = 30
+
+console.log(Math.floor(Math.random(max - min) * 10) + min)   //30-10 wwwe got range of 20 points and then multiplied that random values by 10 and + min 
+
+
