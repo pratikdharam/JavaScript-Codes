d3e94763a20d016edece78bcc0c319362530f4e0
Pratik <134490147+pratikdp03@users.noreply.github.com>
2024-06-19 00:15:56 +0530

    Update arrays.js

--- 02_Basics/arrays.js
+++ 02_Basics/arrays.js
@@ -1,1 +1,39 @@
-console.log("Hello");
+const myArr = [1,2,3,4,5]
+
+// console.log(myArr);
+
+const myArr2 = new Array(1,2,3,4,5)
+
+// console.log(myArr2);
+
+//array mehthods
+
+myArr2.push(6)
+myArr2.pop()
+
+myArr2.unshift(0)
+// myArr2.shift()
+console.log(myArr2);
+
+// console.log(myArr.includes(9));
+// console.log(myArr.indexOf(3));
+
+// const newArr = myArr.join()
+
+// console.log(myArr);
+// console.log( newArr);
+
+
+// slice, splice
+
+console.log("A ", myArr);
+
+const myn1 = myArr.slice(1, 3)
+
+console.log(myn1);
+console.log("B ", myArr);
+
+
+const myn2 = myArr.splice(1, 3)
+console.log("C ", myArr);
+console.log(myn2);
