8d4f5ff0401d4c83c4293abed8076e0c180e9679
Pratik <134490147+pratikdp03@users.noreply.github.com>
2024-06-22 22:47:38 +0530

    added one more color for test

--- 07_projects/01_Color_Changer/script.js
+++ 07_projects/01_Color_Changer/script.js
@@ -57,6 +57,9 @@ buttons.forEach( function(button) {
         if(e.target.id === 'yellow'){
             body.style.backgroundColor = e.target.id
         }
+        if(e.target.id === 'red'){
+            body.style.backgroundColor = e.target.id
+        }
     })
 
 })
